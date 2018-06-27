01e08716205a9d458b9a39f044ea8f31d01388dc
David MacKintosh <dgawmackintosh@gmail.com>
2018-06-27 11:54:59 +0100

    Prevents blank cards and refactors display

--- app.js
+++ app.js
@@ -27,18 +27,26 @@ const cards = [
     data: {
       cards: cards,
       newFront: '',
-      newBack: ''
+      newBack: '',
+      error: false
     },
     methods: {
       flipCard: function(card) {
         card.flipped = !card.flipped;
       },
       addNew: function() {
-        this.cards.push({
-          front: this.newFront,
-          back: this.newBack,
-          flipped: false
-        })
+        if(!this.newFront || !this.newBack) {
+          this.error = true;
+        } else {
+          this.cards.push({
+            front: this.newFront,
+            back: this.newBack,
+            flipped: false
+          });
+          this.newFront = '';
+          this.newBack = '';
+          this.error = false;
+        }
       },
       deleteCard: function(index) {
         cards.splice(index, 1);
